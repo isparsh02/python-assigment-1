2b7dc23490527459fbad27962106d6490e2e45dc
isparsh02 <sprshagnihotri@gmail.com>
2025-10-03 09:38:06 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,14 @@
+
+num1 = float(input(" enter the first number "))
+num2 = float(input(" enter the secound number "))
+
+#perform arithmatic operation
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+print(f"Addition: {addition}")
+print(f"Subtraction: {subtraction}")
+print(f"Multiplication: { multiplication}")
+print(f"Division: {division}")
